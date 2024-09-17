f27253aaf11286dae260a9de8fe8fb640fd488bc
Ari-svg254 <128881947+Ari-svg254@users.noreply.github.com>
2024-09-17 15:45:57 +0700

    Initial commit

---
+++ app/src/main/java/com/example/lab_week_04/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.lab_week_04
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.*
+import com.google.android.material.navigation.NavigationView
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.findNavController
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+            val navController = navHostFragment.navController
+            appBarConfiguration = AppBarConfiguration(
+                setOf(
+                    R.id.listFragment,
+                    R.id.favoritesFragment
+                ),
+                    findViewById(R.id.drawer_layout)
+            )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+            findViewById<NavigationView>(R.id.nav_view)
+                ?.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) ||
+        return super.onSupportNavigateUp()
+    }
+}
